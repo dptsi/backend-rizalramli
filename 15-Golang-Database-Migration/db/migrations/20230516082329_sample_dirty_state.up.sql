ef3c0e13e6076a896834baea0286e2df08df7c68
zalramli <rizalramli993@gmail.com>
2023-05-16 16:00:19 +0700

    update golang database migration

---
+++ 15-Golang-Database-Migration/db/migrations/20230516082329_sample_dirty_state.up.sql
@@ -0,0 +1,20 @@
+CREATE TABLE correct
+(
+    id INT NOT NULL AUTO_INCREMENT,
+    name varchar(255) NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE = InnoDB;
+
+-- CREATE TABLE wrong
+-- (
+--     id INT NOT NULL AUTO_INCREMENT,
+--     name varchir(255) NOT NULL,
+--     PRIMARY KEY (id)
+-- ) ENGINE = InnoDB;
+
+CREATE TABLE wrong
+(
+    id INT NOT NULL AUTO_INCREMENT,
+    name varchar(255) NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE = InnoDB;
